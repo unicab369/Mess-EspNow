f7e57e8bed804e5c5e41d172b1efcb4ca3552dcb
uni369 <unicabpp@yahoo.com>
2024-04-18 21:56:25 -0400

    update.

--- Mess-EspNow/src/main.cpp
+++ Mess-EspNow/src/main.cpp
@@ -1,18 +1,12 @@
 #include <Arduino.h>
+#include "Mess-EspNow.h"
 
-// put function declarations here:
-int myFunction(int, int);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  testMethod();
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
